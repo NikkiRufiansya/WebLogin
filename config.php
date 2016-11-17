64ae9378da63aaa304de43963f5f2df9976535d9
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2016-11-17 21:40:47 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@ define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
 define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
+define('crack', getenv('sekbelajarbang'));
 
 $mysqlCon = mysqli_connect(DB_HOST, DB_USER, DB_PASS, "", DB_PORT) or die("Error: " . mysqli_error($mysqlCon));
 mysqli_select_db($mysqlCon, DB_NAME) or die("Error: " . mysqli_error($mysqlCon));
