89276ea424670ff47a1f82fc12b01f4afed4905a
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2016-11-17 21:34:53 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 <?php
-$host="localhost";
-$user="root";
-$pass="";
-$db="crack";
 
-$kon=mysqli_connect($host,$user,$pass,$db) OR DIE ('Koneksi Gagal');
+// Database Connection Setting
 
+$dbhost = "127.0.0.1"; // Host name 
+$dbport = "3308"; // Host port
+$dbusername = "user"; // Mysql username 
+$dbpassword = "pass"; // Mysql password 
+$db_name = "crack"; // Database name 
+
+$mysqlCon = mysqli_connect($dbhost, $dbusername, $dbpassword, "", $dbport) or die("Error: " . mysqli_error($mysqlCon));
+mysqli_select_db($mysqlCon, $db_name) or die("Error: " . mysqli_error($mysqlCon));
 ?>
